dcfa6c944f67482be4c3efe09c439571fed686ae
Nadia Hossny <202201346@pua.edu.eg>
2024-09-27 21:26:26 +0300

    details (featured,recommended,released)

--- lib/data/end_points.dart
+++
@@ -1,8 +0,0 @@
-class EndPoints {
-  static const String apiKey = '0612cdac7ed1bc2e16f0a795f3142cb0';
-  static const String getPopular = '/3/movie/popular';
-  static const String getUpcoming = '3/movie/upcoming';
-  static const String getTopRated = '/3/movie/top_rated';
-
-  static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500/';
-}
